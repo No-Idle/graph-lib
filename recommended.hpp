9b4b772f3e6962602869c6cc7f81e83aba2bf51c
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-13 23:50:47 +0300

    Update recommended.hpp

--- recommended.hpp
+++ recommended.hpp
@@ -14,9 +14,8 @@ struct Graph {
 
     vector<int> dist;
     vector<vector<int>> ways;
-    vector<bool> points; //pointofcondensing;
-    vector<bool> bridges;
-
+    vector<bool> points; //pointofcondensing
+    
     Graph(int _n) {
         n = _n;
         g = vector<vector<int>>(n);
@@ -30,7 +29,6 @@ struct Graph {
         dist = vector<int>(n, INT_MAX);
         if (w) ways = vector<vector<int>>(n);
         if (po) points = vector<bool>(n);
-        if (br) bridges = vector<bool>(n);
     }
     void clearused() {
         used.clear();
@@ -48,13 +46,13 @@ struct Graph {
         g[u].push_back(v);    
     }
     
-    void dfs(int v);
-    void bfs(int s);
-    void find_bridges(int s);
-    void points_con(int v);
-    void djiksta(int v, int u);
-    void floyd(int v, int u);
-    void condense(int v);
+    void dfs(int);
+    void bfs(int);
+    void find_bridges(int);
+    void points_con(int);
+    void djiksta(int, int);
+    void floyd(int, int);
+    void condense(int);
     bool hascycle();
     int  shortestcycle();
 };
@@ -88,5 +86,30 @@ void readGr(Graph &g) {
     for (int i = 0, a, b; i < m; i++) {
         cin >> a >> b;
         g.addDir(a, b);
+    }
+}
+
+
+int up[100000];
+int depth[100000];
+vector<pair<int, int>> ans;
+
+void Graph::find_bridges(int v, int pr = -1) {
+    used[v] = true;
+    up[v] = depth[v];
+    for (int c : g[v])
+        if (!used[c])
+            depth[c] = depth[v] + 1;
+    for (int to : g[v]) {
+        if (to == pr)
+            continue;
+        if (!used[to])
+            find_bridges(to, v);
+        up[v] = min(up[v], up[to]);
+    }
+    if (up[v] == depth[v] && pr != -1) {
+        if (pr < v)
+            swap(pr, v);
+        ans.push_back({v, pr});
     }
 }
