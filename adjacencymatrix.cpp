3ad761f1fca32d8afd2679fc6431917733d4d843
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-16 20:15:46 +0300

    Update adjacencymatrix.cpp

--- adjacencymatrix.cpp
+++ adjacencymatrix.cpp
@@ -1,1 +1,11 @@
 
+void floyd(int **g, int n)
+/**
+ * g - adjacency matrix, n - vertex count
+ */
+{
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
+}
