304c5fc1ce9f5abf4276e2e47c311e907e488a56
GitProger <51924999+GitProger@users.noreply.github.com>
2020-01-12 13:02:14 +0300

    Update ford_bellman_clear.cpp

--- ford_bellman_clear.cpp
+++ ford_bellman_clear.cpp
@@ -29,3 +29,41 @@ bool fordBellman(vector<int> &d, int start, const vector<edge> &edges)
             return false;
     return true;
 }
+
+
+vector<int> fordBellmanNCyc(vector<int> &d, int start, const vector<edge> &edges)
+/**
+ *  d - distanse array (s -> v)
+ *  edges - all edges in graph
+ *  returns a neg cycle in a graph
+ */
+{
+    vector<int> ans;
+    vector<int> p(n, -1);
+    int n = d.size();
+    for (int i = 0; i < n; i++)
+        d[v] = 1;
+    d[start] = 0;
+
+    for (int i = 0; i < n; i++)
+        for (const edge &E : edges) // E = (u, v)
+            if (d[E.v] > d[E.u] + E.w) {
+                d[E.v] = d[E.u] + E.w;
+                p[v] = u;
+            } 
+    
+    for (const edge &E : edges)
+         if (d[E.v] > d[E.u] + E.w) {
+             int u, v;
+             for (int i = 0; i < n; i++)
+                 v = p[v];
+             u = v;
+             while (u != p[v]) {
+                 ans.push_back(v);
+                 v = p[v];
+             }
+             break;
+         }    
+    return ans;
+}   
+   
