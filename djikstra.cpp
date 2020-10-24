8b31654765c9856e63a35373fbb844d7ed315172
GitProger <51924999+GitProger@users.noreply.github.com>
2020-10-24 20:51:19 +0300

    Update djikstra.cpp

--- djikstra.cpp
+++ djikstra.cpp
@@ -2,6 +2,8 @@ typedef vector<vector<pair<int, int>>> graph;
 constexpr int INF = INT_MAX;
 
 vector<int> djkstra(graph &g, int s, vector<int> &prev)
+/** Complexity: O(n ^ 2 + m)
+ */
 {
     int n = g.size();
     vector<int> d(n, INF);    
@@ -21,10 +23,64 @@ vector<int> djkstra(graph &g, int s, vector<int> &prev)
             int wg = g[v][j].second;
             if (d[v] + wg < d[to]) {
                 d[to] = d[v] + wg;
-                if (prev.size())
-                    prev[to] = v;
+                prev[to] = v;
             }
         }
     }
     return d;
 }
+
+
+vector<int> djkstra_fast(vector<vector<pair<int, int>>> &g, int s, vector<int> &prev)
+/**  Complexity: O(n log(n) + m log(n))
+ */
+{
+    int n = g.size();
+    vector<int> d(n, INF);
+    d[s] = 0;    
+    set<pair<int, int>> unused;
+    for (int i = 0; i < n; i++)
+        unused.insert({d[i], i}); // (val, index)
+    for (int i = 0; i < n; i++) {
+        int v = unused.begin()->second; // index of v
+        unused.erase(unused.begin());
+        for (pair<int, int> &u : g[v]) {
+            int to = u.first, wg = u.second;
+            if (d[v] + wg < d[to]) {
+                unused.erase(unused.find({d[to], to}));
+                d[to] = d[v] + wg;
+                prev[to] = v;
+                unused.insert({d[to], wg});
+            }
+        }
+    }
+    return d;
+}
+
+
+/**
+  n - vertex count
+  m - edges count
+
+  What algorithm should you use?
+  basic: O(n ^ 2 + m)
+  fast: O(n log(n) + m log(n))
+  
+  So, m <= n(n - 1) if there are not multiple edges 
+      (if they are, you should just choose an edge with min weight between each pair of vertexes)
+
+  So, we get an equation:
+    n ^ 2 + m = n log(n) + m log(n)
+  
+  The solution is:
+    m = n(n - log(n)) / (log(n) - 1)
+  
+  So, we have 2 cases:
+     * m < n(n - log(n)) / (log(n) - 1)  ==>
+         fast algorithm is optimal
+     * m > n(n - log(n)) / (log(n) - 1)  ==>
+         basic algorithm is optimal    (for example imagine m ~ n ^ 2, then: first (basic) 
+                                           algorithm is O(n ^ 2) and second (fast) is O(log(n) n ^ 2)
+
+*/
+
